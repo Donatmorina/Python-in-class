94396dfb91c45e479379864007810d36d8bcccdb
Donatmorina <140402284+Donatmorina@users.noreply.github.com>
2023-09-05 21:49:32 +0200

    Add files via upload

---
+++ Domor6924_T2.py
@@ -0,0 +1,151 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py
+142.292 
+
+Skriv inn din setning:
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+142.292
+
+Skriv inn din setning:lol
+gjett hvor mange tegn det er i setning:3
+Thats True
+Gi meg et tall:10
+1.6
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:632
+1254827.504
+
+Skriv inn din setning:ks
+gjett hvor mange tegn det er i setning:2
+Thats True
+Gi meg et tall:5
+8.0
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:5
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 29, in <module>
+    resultat=random_tallditt,_tall
+NameError: name 'random_tallditt' is not defined. Did you mean: 'random_tall'?
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:5
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 30, in <module>
+    print(resultat/ditt_tall)
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:5
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 30, in <module>
+    print(resultat/ditt_tall)
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:5
+1.2
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:7
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 30, in <module>
+    print(resultat/ditt_tall)
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:6
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 30, in <module>
+    print(resultat/ditt_tall)
+TypeError: unsupported operand type(s) for /: 'tuple' and 'int'
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:5
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 29, in <module>
+    resultat=float (random_tall, ditt_tall)
+TypeError: float expected at most 1 argument, got 2
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+Gi meg et tall:4
+13.5
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+142.292
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+142.292
+Arealet til en sirkel med radius 6.73 er: 
+Skriv inn din setning:
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+142.292
+Arealet til en sirkel med din valgte radius er: 
+Skriv inn din setning:
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+Arealet til en sirkel med din valgte radius er:
+142.292
+
+Skriv inn din setning:Always look on the bright side of life
+gjett hvor mange tegn det er i setning:
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:2
+Arealet til en sirkel med din valgte radius er:
+12.566
+
+Skriv inn din setning:ksjdkeks
+Gjett lengden av din setning:8
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 17, in <module>
+    print("Thats", len(setning)==(gjett)+"!!")
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:32
+Arealet til en sirkel med din valgte radius er:
+3216.991d
+d
+Skriv inn din setning:
+Gjett lengden av din setning:dwq
+Traceback (most recent call last):
+  File "C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py", line 15, in <module>
+    gjett=int(input("Gjett lengden av din setning:"))
+ValueError: invalid literal for int() with base 10: 'dwq'
+>>> 
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:213
+Arealet til en sirkel med din valgte radius er:
+142530.917
+
+Skriv inn din setning:ikr
+Gjett lengden av din setning:3
+Thats True !!
+Gi meg et tall: 5
+11.0
+>>> 
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+Arealet til en sirkel med din valgte radius er:
+142.292
+
+Skriv inn din setning:riktig
+Gjett lengden av din setning:6
+ThatsTrue!!
+Gi meg et tall:
+== RESTART: C:\Users\donat\AppData\Local\Programs\Python\Python311\Oppgaver.py =
+skriv inn radius:6.73
+Arealet til en sirkel med din valgte radius er:
+142.292
+
+Skriv inn din setning:riktig
+Gjett lengden av din setning:5
+Thats False!!
+Gi meg et tall:5
+9.0
